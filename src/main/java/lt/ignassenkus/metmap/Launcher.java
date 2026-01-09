ea974c6ab6b41dba61e078dbd369d6e3b231c09a
Ignasellio <126019994+Ignasellio@users.noreply.github.com>
2026-01-09 15:17:01 +0200

    Updated to allow building and using JAR with JRE Java 21.

--- src/main/java/lt/ignassenkus/metmap/Launcher.java
+++ src/main/java/lt/ignassenkus/metmap/Launcher.java
@@ -3,5 +3,5 @@ package lt.ignassenkus.metmap;
 import javafx.application.Application;
 
 public class Launcher {
-    static void main(String[] args) {Application.launch(App.class, args);}
+    public static void main(String[] args) {Application.launch(App.class, args);}
 }
